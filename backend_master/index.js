d9ba403f04a8db2d02038b7eb29bd455fbe537a4
divyadeep10 <divyadeepsundriyal@gmail.com>
2024-11-21 22:48:27 +0530

    backend added for check

--- backend_master/index.js
+++ backend_master/index.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(express.json());  
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend API');
+  });
+
 // Routes
 app.use("/api/auth", authRoutes);
 
