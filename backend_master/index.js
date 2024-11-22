a54b884f08ce7a0153541d285c9da8a745f57fa3
divyadeep10 <divyadeepsundriyal@gmail.com>
2024-11-22 23:38:56 +0530

    progress tracker deleted

--- backend_master/index.js
+++ backend_master/index.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
-const progressRoutes = require("./routes/progressRoutes");
 const errorHandler = require("./middleware/errormiddleware");
 
 dotenv.config();
@@ -23,7 +22,6 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/progress", progressRoutes);
 
 // CORS Middleware - Allowing requests only from your frontend URL
 app.use(cors({
--- backend_master/models/user.js
+++ backend_master/models/user.js
@@ -4,12 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  hasDashboardAccess: { type: Boolean, default: false },
-  solvedQuestions: { type: Number, default: 0 },
-  dailyStreak: { type: Number, default: 0 },
-  accuracy: { type: Number, default: 0 }, // Percentage
-  learningRate: { type: Number, default: 0 }, // Custom metric
-  lastSolvedDate: { type: Date, default: null }, // To track streaks
+  hasDashboardAccess: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("User", userSchema);
