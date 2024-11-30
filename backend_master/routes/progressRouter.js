b485995a7a830f1b8b8eeccb24a7ada6135fbc6a
divyadeep10 <divyadeepsundriyal@gmail.com>
2024-11-30 12:51:19 +0530

    update progress.js 4

--- backend_master/routes/progressRouter.js
+++ backend_master/routes/progressRouter.js
@@ -4,13 +4,6 @@ const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
-// In progressRouter.js
-router.get('/getp/:userId', (req, res, next) => {
-    console.log('Received request for:', req.params.userId);
-    next();
-  });
-
-
 // Route to get progress
 router.get('/getp/:userId', getProgress);
 
