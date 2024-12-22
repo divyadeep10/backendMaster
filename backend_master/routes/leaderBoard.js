ec971e4892ebb9c49a86a9ee41b1a41e4e1f5a9e
divyadeep10 <divyadeepsundriyal@gmail.com>
2024-12-23 00:07:41 +0530

    leaderboard added 3

--- backend_master/routes/leaderBoard.js
+++ backend_master/routes/leaderBoard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Progress = require("./models/Progress");
+const Progress = require("./models/progress");
 
 router.get("/getleaderboard", async (req, res) => {
     try {
